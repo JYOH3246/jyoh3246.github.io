975b5c2c21c092ce71313ffb5f8ae41bf2994f70
재영이 <jeayeong70@gmail.com>
2024-04-18 20:25:29 +0900

    개발환경 재설정

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,20 +1,14 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
-import Quasar from "quasar";
 import router from './router'
 
-// import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
-
-// import quasar css
-import 'quasar/dist/quasar.css'
-
 const app = createApp(App)
-app.use(Quasar, {
-    plugins : {},
-})
+
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
